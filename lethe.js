e84090fe5feffb476826e13073143a81ef4e3527
John smith <cutelestiaisdebestia@hotmail.com>
2016-01-04 22:03:49 -0500

    Update lethe.js

--- lethe.js
+++ lethe.js
@@ -215,7 +215,7 @@ if (m.content.startsWith(`?niger`)) { //niger
 }
 if (m.content.startsWith(`?nigger`)) { //niger
   if (!checkCommand(m,`?nigger`)) return
-  if(m.content.length < 7){
+  if(m.content.length < 8){
     client.reply(m, "This is extremely offensive, racist, and sexist. Labelling someone with the word \"nigger\" is not right. We're all human and skin color, nationality, religion, political beliefs, sexual identity and orientation and lifestyle don't make us different under the skin. Things like this should be banned from tumblr.")
     return;
   } else {
