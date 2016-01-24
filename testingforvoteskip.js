348ae10f70cc991cfe47d684f37f10d5be681a62
John smith <cutelestiaisdebestia@hotmail.com>
2016-01-24 11:10:46 -0500

    Update testingforvoteskip.js

--- testingforvoteskip.js
+++ testingforvoteskip.js
@@ -40,6 +40,7 @@ var voteAllIDs = [];
 var playQueue = [];
 var boundChannel = false;
 var currentStream = false;
+var ownerID = "81526338728501248"
 var voteCount = 0;
 var voteTotalCount = 0;
 // Video that is currently being played
@@ -402,7 +403,27 @@ if (m.content.startsWith(`?lyin`)) { //memecontrol
   client.reply(m, lyinArray[Math.floor(Math.random() * lyinArray.length)])
   return;
 } 
-
+if (m.content.startsWith(`?compliment`)){
+	var complimentArray = ["Hiya! Did you know any day spent with you is my favorite day?","Heya, I just wanted to tell you: when in doubt, smile!","Hey man, my life would suck without you. Thanks, you're great.","Phew! You're back. there’s rarely any dull moment when I’m with you. You're fabulous.", "Whoa, I like your socks. #sockswag ","Hey! I think our friendship is like a cup of tea, a special blend of you and me.","Hi! LOL your status was super funny!","Hey! I think you have a good fashion sense.","Hi! I wanted to let you know that you wear really cute sweaters.","Hi! You're my number one.","Hey, do you wanna build a snowman? It doesn't have to be a snowman~","Hey... I'd give you a tissue, for your issues.","Hi! Just letting you know that you can copy my math homework!","Heya, you're so fancy, you already know.","Hey friend, I'd invite you to my birthday party.","Hi! Just letting you know that your personality is brighter than the stars.","I love you!","Hi! You're spontaneous, and I love it!","Hey, I appreciate all of your opinions.","W-whoa... Your smile is breath taking.","Hi! You are the gravy to my mashed potatoes.","Hey, I'm so glad we met.","Hey, is it hot in here or is it just you?","Hi. I don't speak much English, but with you all I really need to say is beautiful.","Hey, I think you could survive a zombie apocalypse.","Y-you're so rad...","Heya friend! You're more fun than a barrel of monkeys.","Hi! You're nicer than a day on the beach.","Hey, you make me think of beautiful things, like strawberries.","Hi. You're more fun than bubble wrap.","Man, playing video games with you would be fun.","Hello, I'd like to know why you're so beautiful.","Hey, I think you could invent words and people would use them.","Hey, did you know there's 21 letters in the Alphabet? Oh wait. I forgot u, r, a, q, t! How stupid of me... :wink:","Hey! You're tastier than chicken soup when I have the flu.","Hi, are you a Beaver? Cause Dam!","Hey, why are you so talented? I can't Handel your musicality.", "Hi! Are you a train? because I choo,choo,choose you!", "You're beautiful no matter what they say","Hey... You're the apple in my eye, it still hurts but I love pain~", "Hey, are you wifi? Because I'm feeling a connection ;)", "Hey, did you know you are the USB port to my USB? We fit perfectly together.","Hi. Did you know you're the smoothest one of all? <3"]
+	var requestUrl = "http://www.insultgenerator.org/";
+   reequest(requestUrl, function(error, response, html){
+     if(!error){
+       var $ = cheerio.load(html);
+       $('.wrap').filter(function(){
+         var insultData = $(this);
+         randomInsult = insultData.text();
+       })
+     }
+   })
+   var insult = randomInsult
+   if(m.author.id === ownerID){
+   	client.reply(m, complimentArray)
+   	return;
+   } else {
+   	client.reply(m, insult)
+   	return;
+   }
+}
 if (m.content.toLowerCase().startsWith(`${botMention} hi`) || m.content.toLowerCase().startsWith(`${botMention} hello`) || m.content.toLowerCase().startsWith(`${botMention} hey`) || m.content.toLowerCase().startsWith(`${botMention} sup`) || m.content.toLowerCase().startsWith(`${botMention} yo`)) {
   if (m.author.id === "81526338728501248") {
     var complimentArray = ["Hiya! Did you know any day spent with you is my favorite day?","Heya, I just wanted to tell you: when in doubt, smile!","Hey man, my life would suck without you. Thanks, you're great.","Phew! You're back. there’s rarely any dull moment when I’m with you. You're fabulous.", "Whoa, I like your socks. #sockswag ","Hey! I think our friendship is like a cup of tea, a special blend of you and me.","Hi! LOL your status was super funny!","Hey! I think you have a good fashion sense.","Hi! I wanted to let you know that you wear really cute sweaters.","Hi! You're my number one.","Hey, do you wanna build a snowman? It doesn't have to be a snowman~","Hey... I'd give you a tissue, for your issues.","Hi! Just letting you know that you can copy my math homework!","Heya, you're so fancy, you already know.","Hey friend, I'd invite you to my birthday party.","Hi! Just letting you know that your personality is brighter than the stars.","I love you!","Hi! You're spontaneous, and I love it!","Hey, I appreciate all of your opinions.","W-whoa... Your smile is breath taking.","Hi! You are the gravy to my mashed potatoes.","Hey, I'm so glad we met.","Hey, is it hot in here or is it just you?","Hi. I don't speak much English, but with you all I really need to say is beautiful.","Hey, I think you could survive a zombie apocalypse.","Y-you're so rad...","Heya friend! You're more fun than a barrel of monkeys.","Hi! You're nicer than a day on the beach.","Hey, you make me think of beautiful things, like strawberries.","Hi. You're more fun than bubble wrap.","Man, playing video games with you would be fun.","Hello, I'd like to know why you're so beautiful.","Hey, I think you could invent words and people would use them.","Hey, did you know there's 21 letters in the Alphabet? Oh wait. I forgot u, r, a, q, t! How stupid of me... :wink:","Hey! You're tastier than chicken soup when I have the flu.","Hi, are you a Beaver? Cause Dam!","Hey, why are you so talented? I can't Handel your musicality.", "Hi! Are you a train? because I choo,choo,choose you!", "You're beautiful no matter what they say","Hey... You're the apple in my eye, it still hurts but I love pain~", "Hey, are you wifi? Because I'm feeling a connection ;)", "Hey, did you know you are the USB port to my USB? We fit perfectly together.","Hi. Did you know you're the smoothest one of all? <3"]
@@ -553,7 +574,7 @@ if (m.content.startsWith(`?goodgirls`)){ //goodgrils
         playStopped();
         voteTotalCount = 0;
         voteAllIDs = [];
-        totalNumberNeeded = 0;
+      //  totalNumberNeeded = 0;
         return;
       } else {
         console.log("Not breaking limits........")
