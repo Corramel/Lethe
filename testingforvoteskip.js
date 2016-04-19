66975359fe4392b0e1cc994376c8d578d8d8a340
John smith <cutelestiaisdebestia@hotmail.com>
2016-04-18 20:43:52 -0400

    asdf

--- testingforvoteskip.js
+++ testingforvoteskip.js
@@ -187,10 +187,10 @@ client.on('message', m => {
    }
  if (m.content.startsWith(`?fireemblempic`)){
    var requestUrl = "http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=fire_emblem_if&rating=s"
-   reequest(requestUrl, function(error, response, body){
+   reequest(requestUrl, function(error, response, html){
      //console.log(body)
      if(!error){
-       var $ = cheerio.load(body, {xmlMode : true});
+       var $ = cheerio.load(html, {xmlMode : true});
        var toParse = $('posts').text();
        console.log(XMLS.serializeToString(toParse));
        //console.log(toParse);
