521cf7f02a9179f0908f28a8afc2170cb99f3b98
John smith <cutelestiaisdebestia@hotmail.com>
2016-05-09 22:35:01 -0400

    a

--- testingforvoteskip.js
+++ testingforvoteskip.js
@@ -201,7 +201,7 @@ client.on('message', m => {
            var parsingInfo = toParse.text();
 
            parseString(toParse, function(err, result){
-             console.log(result.posts.post);
+             console.log(result.posts.post.length);
              //console.log(lengthOfPosts);
 
                var randomPost = result.posts.post[Math.floor(Math.random()*1)].$
