d2e9d93e2737edf347372877f1d9f98eed7e9839
John smith <cutelestiaisdebestia@hotmail.com>
2016-08-11 18:02:52 -0400

    asdf

--- testingforvoteskip.js
+++ testingforvoteskip.js
@@ -144,9 +144,10 @@ client.on('message', m => {
     return;
   }
   if (m.content.startsWith(`?slash `)){
-    var mentionInMessage = m.mentions;
+    var mentionInMessage = m;
     var cleanUpMessage = m.cleanContent
     console.log(cleanUpMessage);
+    console.log(mentionInMessage);
     var slashArray = ["http://i.imgur.com/RH2yX.jpg", "http://media.giphy.com/media/daDA43pfSyeHK/giphy.gif", "http://i.imgur.com/7JNeSzT.jpg", "http://i.imgur.com/ZgR1USn.png", "http://i.imgur.com/2KTyI5n.png", "http://i.imgur.com/WnHUFcB.jpg", "http://i.imgur.com/34X6qf4.jpg", "http://i.imgur.com/Oy49Zln.png"]
     client.sendMessage(m.channel, `<@!${mentionInMessage}> you have been slashed by ${m.author}! ${slashArray[Math.floor(Math.random()*slashArray.length)]}`);
   }
