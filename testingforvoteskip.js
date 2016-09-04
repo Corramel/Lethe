03898d4db7f4ef2fbe449a0bf648a1fd7b99a51e
John smith <cutelestiaisdebestia@hotmail.com>
2016-09-04 00:34:45 -0400

    asdf

--- testingforvoteskip.js
+++ testingforvoteskip.js
@@ -152,7 +152,8 @@ client.on('message', m => {
     console.log(cardNames);
     console.log(tarots);
     console.log(tarots.Sharpshooter.Card);
-    var chosenCard = cardNames[Math.floor(Math.random()*cardNames.length)]
+    var chosenCard = cardNames[Math.floor(Math.random()*cardNames.length)];
+    console.log(chosenCard);
     client.sendMessage(m.channel, `${tarots.chosenCard.Card}`);
     client.sendMessage(m.channel, `${tarots.cardNames[Math.floor(Math.random()*cardNames.length)].Description}`);
     return;
